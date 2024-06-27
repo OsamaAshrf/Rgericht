ff52bd9624031f6ac2180537a3ce0a06ba4136f1
OsamaAshrf <164712241+OsamaAshrf@users.noreply.github.com>
2024-06-27 06:59:53 +0300

    Update Bigknife.jsx

--- src/Components/Bigknife/Bigknife.jsx
+++ src/Components/Bigknife/Bigknife.jsx
@@ -9,7 +9,7 @@ function Knife(){
     <div className="Knife-container2" id='aboutUs'>
         <div className="Knife-container">
             <Knifedev title="About Us" display="end" deg="0deg"/>
-            <div className="knifeimg"><img src="../Rgericht/public/assets/knife.png"/></div>
+            <div className="knifeimg"><img src="/Rgericht/assets/knife.png"/></div>
             <Knifedev title="Our History" display="start" deg="180deg"/>
         </div>
     </div>
