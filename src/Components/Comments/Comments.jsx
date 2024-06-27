b21664b81cd9dee8f03809d34a98df1838f618b1
OsamaAshrf <164712241+OsamaAshrf@users.noreply.github.com>
2024-06-27 07:04:05 +0300

    Update Comments.jsx

--- src/Components/Comments/Comments.jsx
+++ src/Components/Comments/Comments.jsx
@@ -8,19 +8,19 @@ function Comments(){
     <div className="Comments-container" id="test">
         <Heading head1="Testimony" head2="Happy Customers" dirc="center"/>
         <div className="bioGrid">
-            <Bio pic="../../Rgericht/public/assets/prof1.png"
+            <Bio pic="/Rgericht/assets/prof1.png"
             comment="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas maiores pariatur nulla eum sequi sed. Quod libero, sint, sunt odio ut saepe, doloribus at iusto eligendi labore distinctio inventore consequatur."
             name="Wade Warren"
             jop="Sommelier"/>
-            <Bio pic="../../Rgericht/public/assets/prof2.png"
+            <Bio pic="/Rgericht/assets/prof2.png"
             comment="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas maiores pariatur nulla eum sequi sed. Quod libero, sint, sunt odio ut saepe, doloribus at iusto eligendi labore distinctio inventore consequatur."
             name="Wade Warren"
             jop="Sommelier"/>
-            <Bio pic="../../Rgericht/public/assets/prof3.png"
+            <Bio pic="/Rgericht/assets/prof3.png"
             comment="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas maiores pariatur nulla eum sequi sed. Quod libero, sint, sunt odio ut saepe, doloribus at iusto eligendi labore distinctio inventore consequatur."
             name="Wade Warren"
             jop="Sommelier"/>
-            <Bio pic="../../Rgericht/public/assets/prof4.png"
+            <Bio pic="/Rgericht/assets/prof4.png"
             comment="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas maiores pariatur nulla eum sequi sed. Quod libero, sint, sunt odio ut saepe, doloribus at iusto eligendi labore distinctio inventore consequatur."
             name="Wade Warren"
             jop="Sommelier"/>
